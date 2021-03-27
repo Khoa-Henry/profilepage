dec6c09e1ac1d2e0fc6b0fef9f14f400c6379cae
Khoa Nguyen <knguyen1@amfam.com>
2021-03-26 22:25:48 -0500

    update

--- src/Home.js
+++ src/Home.js
@@ -13,8 +13,8 @@ export default function Home() {
                         <div className="col-10 mx-auto">
                             <div className="row">
                             <div className="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1">
-                                <h1>Hello, I'm a GOLD II TFT Player For {""}<strong className="brand-name">Tier1 Gaming Lounge</strong></h1>
-                                <h3 className="my-3">I have achieve the impossible, getting 3-star Sett & Kayn</h3>
+                                <h1>Hello, I'm a PLATINUM II TFT Player For {""}<strong className="brand-name">Tier1 Gaming Lounge</strong></h1>
+                                <h3 className="my-3">I have achieve the impossible, getting 3-star Sett & Kayn & Yone</h3>
                                 <div className="mt-3">
                                     <Link to="/about" className="btn-get-started">Learn More About Me</Link>
                                     <a href="https://www.linkedin.com/in/khoa-henry-nguyen/" target="_blank" rel="noreferrer" className="btn-get-started" >Linkedin</a>
